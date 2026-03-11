1292306d856be4be20b862a761c804af761461a7
seeraiwer <github.com.g997b@passmail.com>
2026-03-11 19:36:21 +0100

    Suppression de contenu inutile dans le composant Skills pour améliorer la clarté et la lisibilité du code.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -48,15 +48,6 @@ const Skills = () => {
           <img className="h-10" src={selectSkill?.logo} alt={selectSkill?.name} />
           <h6>{selectSkill?.name}</h6>
         </div>
-        <br />
-        <ul className="list-decimal px-4 font-Poppins sm:text-sm text-xs !leading-7">
-          <li>Lorem ipsum dolor sit, amet consectetur adipisicing.</li>
-          <li>Lorem ipsum dolor sit, ame.</li>
-          <li>Lorem ipsum dolor sit, amet consectetur</li>
-          <li>Lorem ipsum dolor sit, amet dolor sit, amet consectetur adipisicing.</li>
-          <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad est beatae quos rem.</li>
-        </ul>
-        <br />
         <div className="flex justify-end">
           <button onClick={closeModal} className="btn">
             Fermer
